6bb44f6097c83afe9cbe1cff0b613707718ccf1b
ChellyL <38887991+ChellyL@users.noreply.github.com>
2022-02-17 12:55:10 +0800

    Create followlist30.py

---
+++ followlist30.py
@@ -0,0 +1,21 @@
+# 将已经获取的关注list处理为每30个id为一组的列表
+
+def getgroup():
+    list = []
+    all = len(list)
+    total = all / 30
+    if type(total) != "int":
+        total = int(all / 30) + 1
+
+    start = 0
+    end = 30
+
+    group30 = []
+    for i in range(8):
+        group = list[start:end]
+        group30.append(group)
+        # print(group)
+        start += 30
+        end += 30
+
+    return bgroup30
